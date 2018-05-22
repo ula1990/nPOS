7b3306c355443a50521ca3d07fec8ff83ca6cc8c
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-05-22 19:49:35 +0200

    Initial Commit

---
+++ nPOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nPOS
+//
+//  Created by Uladzislau Daratsiuk on 22/05/2018.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
