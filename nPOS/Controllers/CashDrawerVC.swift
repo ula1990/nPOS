94426e21902ab3e03213c6139826714632aef76f
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-05-24 16:49:15 +0200

    commit

--- nPOS/Controllers/CashDrawerVC.swift
+++ nPOS/Controllers/CashDrawerVC.swift
@@ -9,26 +9,96 @@
 import UIKit
 
 class CashDrawerVC: UIViewController {
+    
+    lazy var cashDrawerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.shadowRadius = 5
+        view.layer.shadowOpacity = 0.3
+        view.layer.cornerRadius = 5
+        view.backgroundColor = UIColor(named: "background")
+        return view
+    }()
+    
+    
+    lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Cash Drawer was opened"
+        label.textColor = UIColor.white.withAlphaComponent(0.8)
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        return label
+    }()
+    
+    lazy var cashDrawerLogo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "cashdrawer")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.tintColor = UIColor.white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy var dismissButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "dismiss"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.tintColor = UIColor.white
+//        button.contentMode = .scaleAspectFit
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
 
     fileprivate func configureNavBar() {
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "dismiss"), style: .plain, target: self, action:#selector(handleDismiss))
-        navigationItem.rightBarButtonItem?.tintColor = .white
-        navigationItem.title = "Cash Drawer"
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
         navigationController?.navigationBar.tintColor = .white
         
     }
     
+    fileprivate func setupView() {
+        cashDrawerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        cashDrawerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        cashDrawerView.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        cashDrawerView.heightAnchor.constraint(equalToConstant: 280).isActive = true
+        
+        infoLabel.centerXAnchor.constraint(equalTo: cashDrawerView.centerXAnchor).isActive = true
+        infoLabel.bottomAnchor.constraint(equalTo: cashDrawerLogo.topAnchor).isActive = true
+        infoLabel.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        infoLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        cashDrawerLogo.centerXAnchor.constraint(equalTo: cashDrawerView.centerXAnchor).isActive = true
+        cashDrawerLogo.centerYAnchor.constraint(equalTo: cashDrawerView.centerYAnchor).isActive = true
+        cashDrawerLogo.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        cashDrawerLogo.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        
+        dismissButton.centerXAnchor.constraint(equalTo: cashDrawerView.centerXAnchor).isActive = true
+        dismissButton.topAnchor.constraint(equalTo: cashDrawerLogo.bottomAnchor,constant: -5).isActive = true
+        dismissButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        dismissButton.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(named: "background")?.withAlphaComponent(0.5)
+        view.backgroundColor = UIColor(named: "background")?.withAlphaComponent(0)
+        view.addSubview(cashDrawerView)
+        cashDrawerView.addSubview(cashDrawerLogo)
+        cashDrawerView.addSubview(dismissButton)
+        cashDrawerView.addSubview(infoLabel)
         configureNavBar()
+        setupView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        view.backgroundColor = UIColor(named: "background")
+        view.backgroundColor = UIColor(named: "background")?.withAlphaComponent(0)
         configureNavBar()
         
     }
---
+++ nPOS/Models/GiftCard.swift
@@ -0,0 +1,25 @@
+//
+//  GiftCard.swift
+//  nPOS
+//
+//  Created by Uladzislau Daratsiuk on 24/05/2018.
+//  Copyright © 2018 Uladzislau Daratsiuk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GiftCard: NSObject {
+    var id: String?
+    var name: String?
+    var imageName: String?
+    var percentage: Double?
+    
+    init(id: String, name: String,imageName: String, percentage: Double){
+        self.id = id
+        self.name = name
+        self.imageName = imageName
+        self.percentage = percentage
+    }
+    
+}
